80ffa024564bc44b4d963f1e2dcab3b92e2796e8
FartGarfild <40548064+FartGarfild@users.noreply.github.com>
2024-07-29 00:11:41 +0300

    Update fastpanel_deployer.sh
    Був баг з розпакованими архівами в звязку з особливостями резервного копіювання панелі.

--- fastpanel_deployer.sh
+++ fastpanel_deployer.sh
@@ -1,50 +1,59 @@
 #!/bin/bash
 
-# Змінні
-SITE_ARCHIVE_DIR="/var/www/fastuser/data/backup/2024.07.27_20-21-28_test_91.247.37.42/" # Сюди прописати шлях до резервної копії
-DB_ARCHIVE_DIR="$SITE_ARCHIVE_DIR"
-# Користувач FastPanel у якому будть створені усі сайти
+# Директории с архивами
+SITE_ARCHIVE_DIR="/var/www/fastuser/data/backup/2024.07.27_20-21-28_test_91.247.37.42/"  # Директория с архивами сайтов
+DB_ARCHIVE_DIR="/var/www/fastuser/data/backup/2024.07.27_20-21-28_test_91.247.37.42/"    # Директория с архивами баз данных
+
+# Имя пользователя для всех сайтов
 USER="fastuser"
 
+# Файл для сохранения паролей баз данных
 DB_PASSWORD_FILE="db_passwords.txt"
 
+# Функция для извлечения имени без расширения
 extract_name() {
   local filename=$(basename -- "$1")
   local name="${filename%.*.*}"
   echo "$name"
 }
 
-# Де буде створено файл з паролями від БД.
+# Удаление старого файла с паролями и создание нового
 rm -f /home/$DB_PASSWORD_FILE
 touch /home/$DB_PASSWORD_FILE
 
+# Обработка архивов сайтов
 for site_archive in "$SITE_ARCHIVE_DIR"/*.tar.gz; do
   DOMAIN=$(extract_name "$site_archive")
-  SITE_DIR="/var/www/$USER/data/www/$DOMAIN/"
+  mkdir /var/www/$USER/data/www2 # тимчасова директорія, тому що FastPanel створює в архіві директорію з повним шляхом.
+  SITE_DIR="/var/www/$USER/data/www2/$DOMAIN/"
+  mv /var/www/$USER/data/www2/*/var/www/*/data/www/* /var/www/$USER/data/www/
+  echo "Создание сайта: $DOMAIN"
   
-  echo "Створення сайту: $DOMAIN"
-  
+  # Создание директории сайта и распаковка файлов
   mkdir -p "$SITE_DIR"
   tar -xzvf "$site_archive" -C "$SITE_DIR"
   
- # Додавання сайту в FastPanel
+ # Создание сайта в FastPanel
    /usr/local/fastpanel2/fastpanel sites create --server-name="$DOMAIN" --owner="$USER"
 done
 
+# Обработка архивов баз данных
 for db_archive in "$DB_ARCHIVE_DIR"/*.sql.gz; do
   DB_NAME=$(extract_name "$db_archive")
   DB_USER="$DB_NAME"
   DB_PASSWORD=$(openssl rand -base64 12)
   
-  echo "Створення БД: $DB_NAME"
+  echo "Создание базы данных: $DB_NAME"
  
- #Додавання БД в панель
+ #Создание БД через панель
   /usr/local/fastpanel2/fastpanel databases create --server=1 --name="$DB_NAME" --username="$DB_USER" --password="$DB_PASSWORD"
   
+ # Импорт базы данных из архива .sql.gz
   zcat "$db_archive" | mysql -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME"
   
- #  Запис паролів в файл
-  echo "Database: $DB_NAME, User: $DB_USER, Password: $DB_PASSWORD" >> $DB_PASSWORD_FILE
+ # Запись информации о базе данных и пароле в файл
+  echo "Database: $DB_NAME, User: $DB_USER, Password: $DB_PASSWORD" >> /home/$DB_PASSWORD_FILE
 done
 
-echo "Готово. Всі саайти створено, паролі від БД можете знайти за шляхом /home/$DB_PASSWORD_FILE."
+echo "Все сайты и базы данных успешно созданы. Пароли сохранены в $DB_PASSWORD_FILE."
+
